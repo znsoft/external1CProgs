fe89ba9148699941aa3ce56e9b29177e570811ff
Eugenie <zn@bk.ru>
2015-04-03 18:52:32 +1000

    переработал метафункции парсинга функций :)

--- АнализаторКода1С/1SCodeAnalyze/1SCodeAnalyze/Структуры/Модуль.cs
+++ АнализаторКода1С/1SCodeAnalyze/1SCodeAnalyze/Структуры/Модуль.cs
@@ -13,7 +13,7 @@ namespace _1SCodeAnalyze.Структуры
         public List<ИнформацияАнализа> ТаблицаАнализа;
         public String Текст;
         public Boolean ЕстьОшибки;
-        //public static Dictionary<String, Boolean> Методы;
+        public static Dictionary<String, Boolean> МетодыМодуля;
 
         public Модуль(FileInfo file)
         {
@@ -21,14 +21,22 @@ namespace _1SCodeAnalyze.Структуры
             Текст = СчитатьСодержимоеФайла(file);
             ЕстьОшибки = false;
             ТаблицаАнализа = new List<ИнформацияАнализа>();
-          //  Методы = new Dictionary<String, Boolean>();
+            МетодыМодуля = new Dictionary<String, Boolean>();
         }
 
-		public Модуль ДобавитьПроблему(String Проблема, int Index){
-			ТаблицаАнализа.Add(new ИнформацияАнализа(Index, Проблема, Проблема));
-			ЕстьОшибки = true;
-			return this;
-		}
+        public Модуль ДобавитьПроблему(String Проблема, int Index)
+        {
+            ТаблицаАнализа.Add(new ИнформацияАнализа(Index, Проблема, Проблема));
+            ЕстьОшибки = true;
+            return this;
+        }
+
+        public Модуль ДобавитьПроблему(ИнформацияАнализа Проблема)
+        {
+            ТаблицаАнализа.Add(Проблема);
+            ЕстьОшибки = true;
+            return this;
+        }
 
         private String СчитатьСодержимоеФайла(FileInfo file)
         {
@@ -36,10 +44,27 @@ namespace _1SCodeAnalyze.Структуры
             return Str.ReadToEnd();
         }
 
+        public ИнформацияАнализа ПрямойЗапрос(String Текст, int Index)
+        {
+            var ПоискЗапроса = new Regex(@"^(?!\/\/)[^\/]*?\.(выполнить|найтипокоду|найтипореквизиту|найтипонаименованию)[\s]?\(", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+            Match Найдены = ПоискЗапроса.Match(Текст);
+            if (!Найдены.Success)
+                return null;
+            return new ИнформацияАнализа(Найдены.Index + Index, Найдены.Groups[1].Value, Найдены.Groups[1].Value);
+        }
+
+        public Boolean ЕстьЗапрос(String Текст, int Index)
+        {
+            ИнформацияАнализа Проблема = ПрямойЗапрос(Текст, Index);
+            if (Проблема == null)
+                return false;
+            ДобавитьПроблему(Проблема);
+            return true;
+        }
+
         private void НайтиВсеФункцииИПроцедуры(String ИсходныйКод)
         {
-            //var ПоискФункций = new Regex(@"^(?!\/\/)[^\.\/]*?(procedur|functio|Процедур|Функци)[enая][\s]*?([А-Яа-яa-z0-9_]*?[\s]?\()([\S\s]*?)Конец\1[enыи]", RegexOptions.IgnoreCase | RegexOptions.Multiline);
-            var ПоискФункций = new Regex(@"^(?!\/\/)[^\.\/]*?(procedur|functio|Процедур|Функци)[enая][\s]*?([А-Яа-яa-z0-9_]*?[\s]?\()[\S\s]*?(\/\/)?.*?\.(выполнить|найтипокоду|найтипореквизиту|найтипонаименованию)[\s]?\([\S\s]*?(Конец|End)\1[enыи]", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+            var ПоискФункций = new Regex(@"^(?!\/\/)[^\.\/]*?(procedur|functio|Процедур|Функци)[enая][\s]*?([А-Яа-яa-z0-9_]*?[\s]?\()[\S\s]*?(Конец|End)\1[enыи]", RegexOptions.IgnoreCase | RegexOptions.Multiline);
             MatchCollection Найдены = ПоискФункций.Matches(ИсходныйКод);
             foreach (Match Функция in Найдены)
             {
